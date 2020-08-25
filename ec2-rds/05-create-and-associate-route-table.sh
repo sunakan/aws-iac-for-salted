75a72374f22f4eb189f977ca13450f5eb92c69fe
sunakan <sunakan03@gmail.com>
2020-08-25 00:59:43 +0000

    route tableまで

--- ec2-rds/05-create-and-associate-route-table.sh
+++ ec2-rds/05-create-and-associate-route-table.sh
@@ -37,7 +37,7 @@
 # INPUT_JSON + \
 # {
 #   "custom_route_table": {
-#     "route_table_id": "-xxxxxx"
+#     "custom_route_table_id": "rtb-xxxxxx"
 #   }
 # }
 # ----
@@ -67,11 +67,16 @@ export AWS_DEFAULT_REGION=$(echo ${INPUT} | jq --raw-output '.region')
 ################################################################################
 # Main
 ################################################################################
-readonly route_table_id=$(aws ec2 create-route-table --vpc-id ${VPC_ID} --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=${CUSTOM_ROUTE_TABLE_NAME}}]" | jq --raw-output '.RouteTable.RouteTableId')
-aws ec2 create-route --route-table-id ${route_table_id} --destination-cidr-block 0.0.0.0/0 --gateway-id ${INTERNET_GATEWAY_ID} > /dev/null
-echo ${INPUT} | jq --raw-output '.vpc_public_subnets.subnets[].subnet_id' | while read subnet_id; do
-  aws ec2 associate-route-table --subnet-id ${subnet_id} --route-table-id ${route_table_id} > /dev/null
-  aws ec2 modify-subnet-attribute --subnet-id ${subnet_id} --map-public-ip-on-launch > /dev/null
-done
+readonly route_table_count=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=${VPC_ID} | jq '.RouteTables | length')
+if [ ${route_table_count} -lt 1 ]; then
+  local readonly route_table_id=$(aws ec2 create-route-table --vpc-id ${VPC_ID} --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=${CUSTOM_ROUTE_TABLE_NAME}}]" | jq --raw-output '.RouteTable.RouteTableId')
+  aws ec2 create-route --route-table-id ${route_table_id} --destination-cidr-block 0.0.0.0/0 --gateway-id ${INTERNET_GATEWAY_ID} > /dev/null
+  # associate to each subnet
+  echo ${INPUT} | jq --raw-output '.vpc_public_subnets.subnets[].subnet_id' | while read subnet_id; do
+    aws ec2 associate-route-table --subnet-id ${subnet_id} --route-table-id ${route_table_id} > /dev/null
+    aws ec2 modify-subnet-attribute --subnet-id ${subnet_id} --map-public-ip-on-launch > /dev/null
+  done
+fi
 
-echo ${INPUT} | jq ".custom_route_table |= .+ {\"custom_route_table_id\": \"${route_table_id}\"}"
+readonly route_table_id=$(aws ec2 describe-route-tables --filters Name=vpc-id,Values=${VPC_ID} --filter Name=association.main,Values=false | jq '.RouteTables[].RouteTableId')
+echo ${INPUT} | jq ".custom_route_table |= .+ {\"custom_route_table_id\": ${route_table_id}}"
