d98cfbdb5d5fb2d15da73ed687ca0629f92b1efb
sunakan <sunakan03@gmail.com>
2020-08-29 12:39:27 +0000

    ルートテーブルの作成

---
+++ ec2-rds-2/create-custom-route-table.mk
@@ -0,0 +1,25 @@
+include ./shared-variables.mk
+
+.PHONY: create-custom-route-table
+create-custom-route-table: init-variables-for-create-custom-route-table
+	make --no-print-directory output-custom-route-table-if-created > /dev/null 2>&1 \
+	|| aws ec2 create-route-table --vpc-id $(VPC_ID) --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=$(CUSTOM_ROUTE_TABLE_TAG_NAME)}]" > /dev/null
+	make --no-print-directory output-custom-route-table-if-created
+
+.PHONY: output-custom-route-table-if-created
+output-custom-route-table-if-created: init-variables-for-create-custom-route-table
+	$(eval CUSTOM_ROUTE_TABLE := $(shell aws ec2 describe-route-tables --filters Name=vpc-id,Values=$(VPC_ID) | jq --compact-output '.RouteTables[] | select(.RouteTableId != "$(MAIN_ROUTE_TABLE_ID)")'))
+	@[ -n "$(CUSTOM_ROUTE_TABLE)" ] && ( echo '$(CUSTOM_ROUTE_TABLE)' | jq > $(CUSTOM_ROUTE_TABLE_JSON_PATH) )
+
+.PHONY: init-variables-for-create-custom-route-table
+init-variables-for-create-custom-route-table:
+	$(eval VPC_ID                      := $(shell cat $(VPC_JSON_PATH) | jq --compact-output --raw-output 'select(.VpcId!=null).VpcId'))
+	$(eval CUSTOM_ROUTE_TABLE_TAG_NAME := $(shell cat input.json       | jq --compact-output --raw-output '.custom_route_table | select(.name!=null).name'))
+	$(eval MAIN_ROUTE_TABLE_ID         := $(shell aws ec2 describe-route-tables --filters Name=vpc-id,Values=$(VPC_ID) Name=association.main,Values=true | jq --compact-output --raw-output '.RouteTables[] | select(.RouteTableId).RouteTableId'))
+	@[ -n "$(VPC_ID)" ] && [ -n "$(CUSTOM_ROUTE_TABLE_TAG_NAME)" ] && [ -n "$(MAIN_ROUTE_TABLE_ID)" ]
+
+.PHONY: delete-custom-route-table
+delete-custom-route-table: init-variables-for-create-custom-route-table
+	aws ec2 describe-route-tables --filters Name=vpc-id,Values=$(VPC_ID) \
+		| jq --compact-output '.RouteTables[] | select(.RouteTableId != "$(MAIN_ROUTE_TABLE_ID)") | .RouteTableId' \
+		| xargs -I {route-table-id} aws ec2 delete-route-table --route-table-id {route-table-id}
