c557900380db8daf12ac59359c091452426e01cf
sunakan <sunakan03@gmail.com>
2020-08-29 23:07:26 +0000

    add input.json

--- ec2-rds-2/create-custom-route-table-for-public.mk
+++ ec2-rds-2/create-custom-route-table-for-public.mk
@@ -12,7 +12,7 @@ output-custom-route-table-for-public-if-created: init-variables-for-create-custo
 	@[ -n "$(CUSTOM_ROUTE_TABLE)" ] && ( echo '$(CUSTOM_ROUTE_TABLE)' | jq > $(CUSTOM_ROUTE_TABLE_FOR_PUBLIC_JSON_PATH) )
 
 .PHONY: init-variables-for-create-custom-route-table-for-public
-init-variables-for-create-custom-route-table-for-public:
+init-variables-for-create-custom-route-table-for-public: input.json
 	$(eval VPC_ID                      := $(shell cat $(VPC_JSON_PATH) | jq --compact-output --raw-output 'select(.VpcId!=null).VpcId'))
 	$(eval CUSTOM_ROUTE_TABLE_TAG_NAME := $(shell cat input.json       | jq --compact-output --raw-output '.custom_route_table | select(.name!=null).name'))
 	$(eval MAIN_ROUTE_TABLE_ID         := $(shell aws ec2 describe-route-tables --filters Name=vpc-id,Values=$(VPC_ID) Name=association.main,Values=true | jq --compact-output --raw-output '.RouteTables[] | select(.RouteTableId).RouteTableId'))
