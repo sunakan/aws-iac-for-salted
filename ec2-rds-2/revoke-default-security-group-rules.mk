65000e4b6b893c1837beb652117a03084c09c701
sunakan <sunakan03@gmail.com>
2020-08-29 00:38:16 +0000

    変数名を小文字から大文字に変更する

--- ec2-rds-2/revoke-default-security-group-rules.mk
+++ ec2-rds-2/revoke-default-security-group-rules.mk
@@ -3,17 +3,17 @@ include ./shared-variables.mk
 # 特に標準出力しない
 .PHONY: revoke-default-security-group-rules
 revoke-default-security-group-rules: init-variables-for-revoke-default-security-group-rules ## VPC作成時、デフォルトセキュリティグループのルールを削除
-	$(eval security-group-id := $(shell aws ec2 describe-security-groups --filters Name=vpc-id,Values=$(vpc-id) | jq --raw-output '.SecurityGroups[] | select(.GroupName = "default" ) | .GroupId'))
-	aws ec2 describe-security-groups --group-id $(security-group-id) \
+	$(eval SECURITY_GROUP_ID := $(shell aws ec2 describe-security-groups --filters Name=vpc-id,Values=$(VPC_ID) | jq --raw-output '.SecurityGroups[] | select(.GroupName = "default" ) | .GroupId'))
+	aws ec2 describe-security-groups --group-id $(SECURITY_GROUP_ID) \
 		| jq --raw-output --compact-output '.SecurityGroups[].IpPermissions' \
 		| awk '$$0!="[]"' \
-		| xargs --delimiter "\n" -I {ip-permissions} aws ec2 revoke-security-group-ingress --group-id $(security-group-id) --ip-permissions '{ip-permissions}'
+		| xargs --delimiter "\n" -I {ip-permissions} aws ec2 revoke-security-group-ingress --group-id $(SECURITY_GROUP_ID) --ip-permissions '{ip-permissions}'
 	aws ec2 describe-security-groups --group-id $(security-group-id) \
 		| jq --raw-output --compact-output '.SecurityGroups[].IpPermissionsEgress' \
 		| awk '$$0!="[]"' \
-		| xargs --delimiter "\n" -I {ip-permissions-egress} aws ec2 revoke-security-group-egress --group-id $(security-group-id) --ip-permissions '{ip-permissions-egress}'
+		| xargs --delimiter "\n" -I {ip-permissions-egress} aws ec2 revoke-security-group-egress --group-id $(SECURITY_GROUP_ID) --ip-permissions '{ip-permissions-egress}'
 
 .PHONY: init-variables-for-revoke-default-security-group-rules
 init-variables-for-revoke-default-security-group-rules:
-	$(eval vpc-id := $(shell cat $(VPC_JSON_PATH) | jq --compact-output --raw-output '.VpcId'))
-	@[ -n "$(vpc-id)" ] || false
+	$(eval VPC_ID := $(shell cat $(VPC_JSON_PATH) | jq --compact-output --raw-output '.VpcId'))
+	@[ -n "$(VPC_ID)" ] || false
